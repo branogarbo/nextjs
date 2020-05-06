42e2bc2512366e4b502be256bf7e29baca4ccab7
Brian Longmore <brianl.ext@gmail.com>
2020-05-05 21:04:08 -1000

    marker gets added to markers array on click but doesnt get rendered on map

--- sandbox/components/googlemaps.js
+++ sandbox/components/googlemaps.js
@@ -1,26 +1,60 @@
 import { Map as GoogleMap, GoogleApiWrapper, Marker } from 'google-maps-react'
+import { useState } from 'react'
+
+let mapStyle = {
+   width: '800px',
+   height: '600px'
+};
+
+let containerStyle = {
+   width: mapStyle.width,
+   height: mapStyle.height,
+   position: 'relative'
+};
 
 function Map({google}) {
-   let mapStyle = {
-      width: '800px',
-      height: '600px'
-   };
+   let [markers,setMarkers] = useState([
+      {
+         position: {
+            lat: 21.1,
+            lng: -157.1
+         }
+      },
+      {
+         position: {
+            lat: 21,
+            lng: -157
+         }
+      },
+      {
+         position: {
+            lat: 20,
+            lng: -156
+         }
+      }
+   ]);
 
-   let containerStyle = {
-      width: mapStyle.width,
-      height: mapStyle.height,
-      position: 'relative'
+   let addMarker = (ref, map, event) => {
+      markers.push({
+         position: {
+            lat: event.latLng.lat(),
+            lng: event.latLng.lng()
+         }
+      });
+
+      setMarkers(markers);
    };
 
    return (
       <GoogleMap
       zoom={8}
       initialCenter={{ lat: 21, lng: -158 }}
+      onClick={addMarker}
       google={google}
       style={mapStyle}
       containerStyle={containerStyle}>
 
-         <Marker lat={21} lng={-158} />
+         {markers.map(marker => <Marker position={{lat:marker.position.lat,lng:marker.position.lng}} />)}
 
       </GoogleMap>
    );
